ae0528a4240d223ab35f5e84bb122aee0f589053
ihavenolife <matuswall@gmail.com>
2022-08-31 14:25:39 +0000

    templates done

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/podrobnosti")
+def podrobnosti():
+    return render_template("podrobnosti.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
