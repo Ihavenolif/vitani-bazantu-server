d47034a9b12557ce1425df0078c61f4e0d4b3026
ihavenolife <matuswall@gmail.com>
2022-12-27 13:14:26 +0100

    conflict resolve

---
+++ data_parser.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+const { exit } = require("process")
+
+const fileContent = fs.readFileSync("drawing_data_final.txt", {encoding: "utf-8"})
+
+const result = []
+
+for(x of fileContent.split(";\n")){
+    try {
+        result.push(JSON.parse(x.replace("\"border\":\"#999\"", "\"border\":\"#ccc\"")))
+    } catch (error) {
+        
+    }
+    
+}
+
+console.log(JSON.stringify(result))
