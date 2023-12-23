859e909a4aeb31e4e1eea13cc978197be0fa989c
wallmatu <wallmatu@fit.cvut.cz>
2023-12-23 13:50:28 +0100

    suvbot

--- suvbot-counter.js
+++ suvbot-counter.js
@@ -1,35 +1,20 @@
 const express = require("express")
 const app = express()
 cors = require("cors")
-bodyParser = require("body-parser")
-const fs = require("fs")
-const LZString = require("./lz-string-node")
 
 let gamesRemaining = 12;
 
-/**
- * @type {String}
- */
-const dataString = fs.readFileSync("./result_json.json", "utf-8")
-
-/**
- * @type {Array<Array<String>>}
- */
-const data = JSON.parse(dataString)
-
-
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
 
 app.get("/", function (req, res) {
-    console.log(req.socket.remoteAddress)
-
     res.send(String(gamesRemaining--))
 })
 
-app.post("/getMap", function (req, res) {
-    res.send(LZString.compressToUTF16(dataString))
+app.get("/setCount", function (req, res) {
+    if(!req.query.count) res.send("Invalid data");
+    count = Number(req.query.count)
+    gamesRemaining = count;
+    res.send(String(count))
 })
 
 const PORT = 6969
